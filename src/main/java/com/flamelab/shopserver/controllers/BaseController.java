1ebafb1e519541036fa839423e7c22c7878987af
LordMagdara <magicfess@gmail.com>
2023-06-29 19:17:48 +0300

    Fix CORS exception 2

--- src/main/java/com/flamelab/shopserver/controllers/BaseController.java
+++ src/main/java/com/flamelab/shopserver/controllers/BaseController.java
@@ -2,7 +2,7 @@ package com.flamelab.shopserver.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:8052")
+@CrossOrigin(origins = "http://localhost:3000")
 public abstract class BaseController {
 
 }
